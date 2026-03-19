666fafe9304ab28be20eae461576121af930fcfa
Youhyun01 <youhyun0609@naver.com>
2026-03-19 14:38:07 +0900

    week03 v.04

--- week02.py
+++ week02.py
@@ -1,4 +1,5 @@
-import statistics #통계모듈 (각종 통계함수를 제공)
+import statistics #통계모듈 (각종 통계함수를 제공), 파이썬 내장 모듈 사용으로 속도가 느림
 scores = [100, 97, 88, 91]
 average = statistics.mean(scores)
-print(average)
+print(average
+      )
