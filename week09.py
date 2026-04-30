f2209032407ee0ed7a2c7fb87a9153993e9971c0
Ruming01 <youhyun0609@naver.com>
2026-04-30 15:48:47 +0900

    week09 pandas v0.4

--- week09.py
+++ week09.py
@@ -2,12 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-df = pd.DataFrame({'국':[1, 4, 7], '영':[2, 5, 8], '수':[3, 6, 9], '미세먼지':[10, 3, 11]}, index=[1, 2, 3])
+df = pd.DataFrame({'국':[1, 4, 7], '영':[2, 5, 8], '수':[3, 6, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
 print(df)
-df.drop(columns=['수', '미세먼지'])
 
-df_new = ((pd.melt(df)
-          .rename(columns={'variable': 'var', 'value': 'val'})
-          .query('val > 5'))
-          .sort_values(by='val', ascending=False))
+# df_new = df.iloc[1:]
+df_new = df.iloc[1:, 2:]
 print(df_new)
