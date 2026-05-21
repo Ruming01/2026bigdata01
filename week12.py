f4459aba3c06e8f413853c2d618c5e055eecaf3e
Ruming01 <youhyun0609@naver.com>
2026-05-21 16:20:57 +0900

    week12 pandas v1.7

--- week12.py
+++ week12.py
@@ -1,15 +1,17 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 titanic = sns.load_dataset("titanic")
-print(titanic[['alive', 'survived', 'class', 'pclass', 'embarked', 'embark_town']])
-titanic01 = titanic.drop(columns=['embark_town', 'alive', 'class', 'deck'])
+titanic01 = titanic.drop(columns=['embarked', 'alive', 'class', 'deck'])
 # print(titanic01.info())
-# print(titanic01['pclass'].value_counts())
-# print(titanic01[titanic01['pclass'] == 3])
+# print(titanic01.isnull().sum())
+titanic01 = titanic01.dropna(subset=['embark_town'])
+# print(titanic01.info())
+# print(titanic01['age'].mean())
+# print(titanic01['age'].median())
+# print(titanic01['age'].mode())
+titanic01['age'] = titanic01['age'].fillna(titanic01['age'].median())
+print(titanic01.info())
 
-# survival_rate_pclass = titanic01.groupby('pclass')['survived'].mean()
-# print(survival_rate_pclass)
-# survival_rate_sex = titanic01.groupby('sex')['survived'].mean()
-# print(survival_rate_sex)
-# survival_rate_who = titanic01.groupby('who')['survived'].mean()
-# print(survival_rate_who)
+sns.barplot(data=titanic01, x='pclass', y = 'survived')
+plt.show()
