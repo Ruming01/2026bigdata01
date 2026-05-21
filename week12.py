94603ed5d59dcbd1fe6d1aa20ca8036304add5e9
Ruming01 <youhyun0609@naver.com>
2026-05-21 15:05:49 +0900

    week12 pandas v1.5

---
+++ week12.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+
+titanic = sns.load_dataset("titanic")
+print(titanic.head())
+print(titanic.describe())
+print(titanic.info())
+# print(titanic['who'].value_counts())
+print(titanic[['embarked', 'embark_town']])
